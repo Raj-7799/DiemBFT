6c487afc5f8b949f8280d7bf4ea0d4e232733d12
Prince Kumar Maurya <prince06.1996@rediffmail.com>
2021-10-09 13:17:39 -0500

    Added auto build for da based modules

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='diembft',
+    version='0.0.1',
+    description='Implementation of DiemBft paper ',
+    url='',
+    author='Raj Vishal Prince',
+    author_email='',
+    license='MIT',
+    packages=['crypto','quourum','vote','client','diem_core'],
+    zip_safe=False
+)
