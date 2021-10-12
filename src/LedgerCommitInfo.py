65a7e58a23826924fc8c78a4f7efc4989b8bccb7
Vishal Singh <vishals706@gmail.com>
2021-10-12 20:42:54 +0000

    Simplifying TC

--- src/LedgerCommitInfo.py
+++ src/LedgerCommitInfo.py
@@ -1,4 +1,6 @@
+import Util
+
 class LedgerCommitInfo:
     def __init__(self, commit_state_id, vote_info_hash):
         self.commit_state_id = commit_state_id
-        self.vote_info_hash = vote_info_hash
+        self.vote_info_hash = Utils.hash(vote_info_hash)
--- src/VoteInfo.py
+++ src/VoteInfo.py
@@ -1,6 +1,6 @@
 class VoteInfo:
 
-    def __init__(self, id, round: int, parent_id, parent_round: int, exec_state_id):
+    def __init__(self, id: str, round: int, parent_id: str, parent_round: int, exec_state_id: str):
         self.id = id
         self.round = round
         self.parent_id = parent_id
