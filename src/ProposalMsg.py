a02a29054bbd27c3f0834eac0df67a79a1b33844
Raj-7799 <raj1471997@gmail.com>
2021-10-12 20:14:10 +0000

    Removing unwanted comment

--- src/ProposalMsg.py
+++ src/ProposalMsg.py
@@ -7,6 +7,6 @@ class ProposalMsg ():
     def __init__(self, block: Block, last_round_tc: TC, high_commit_qc: QC, pvt_key, pbc_key):
         self.block = block
         self.last_round_tc = last_round_tc
-        self.high_commit_qc = high_commit_qc #Implement sender<-u
+        self.high_commit_qc = high_commit_qc
         self.signature = Util.sign_object(self.block.id, pvt_key, pbc_key)
 
