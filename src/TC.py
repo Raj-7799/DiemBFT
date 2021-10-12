37d139aa9b4a05b414dcd8ce879ae4c0c3168e25
Vishal Singh <vishals706@gmail.com>
2021-10-12 20:58:26 +0000

    Simplyfying TC

--- src/TC.py
+++ src/TC.py
@@ -1,6 +1,6 @@
 class TC:
 #Since TC is used by Pacemaker module only, Its contruction requires all datafields to be declared
-    def __init__(self, round: int):
-        self.round = round
-        self.tmo_high_qc_rounds = []
-        self.tmo_signatures = []
+    def __init__(self, roundNo: int, tmo_high_qc_rounds: list, tmo_signatures: list):
+        self.roundNo = roundNo
+        self.tmo_high_qc_rounds = tmo_high_qc_rounds
+        self.tmo_signatures = tmo_signatures
