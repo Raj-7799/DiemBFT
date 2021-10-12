94bbc1628c86314fed7613607a75fe22e65c58f9
Vishal Singh <vishals706@gmail.com>
2021-10-12 20:52:58 +0000

    Correcting ledger commit info again

--- src/LedgerCommitInfo.py
+++ src/LedgerCommitInfo.py
@@ -1,6 +1,7 @@
 import Util
+import VoteInfo
 
 class LedgerCommitInfo:
-    def __init__(self, commit_state_id: str, vote_info_hash: str):
+    def __init__(self, commit_state_id: str, vote_info: VoteInfo):
         self.commit_state_id = commit_state_id
-        self.vote_info_hash = Util.hash(vote_info_hash)
+        self.vote_info_hash = Util.hash(vote_info)
