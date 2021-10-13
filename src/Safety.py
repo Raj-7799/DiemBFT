237dba0bfa75005f3b8b2d5d2141cd7e2eaa6e5f
Raj-7799 <raj1471997@gmail.com>
2021-10-13 00:39:11 +0000

    making changes to safety

--- src/Safety.py
+++ src/Safety.py
@@ -1,44 +1,45 @@
-import LedgerCommitInfo as LedgerCommitInfo
-import VoteInfo as VI
-import VoteMsg as VoteMsg
+import BlockTree as bt
 import TimeoutInfo as Timeoutinfo
 
 
 class Safety():
 
-    def __init__(self, ledger, blocktree):
-        self._ledger = ledger
-        self._blocktree = blocktree
-        self._private_key = None
-        self._public_keys = None
-        self._highest_vote_round = None
-        self._highest_qc_round = None
+    def __init__(self, ledger, blocktree, private_key, public_keys, highest_vote_round, highest_qc_round, sender, pvt_key, pbc_key):
+        self.ledger = ledger
+        self.blocktree = blocktree
+        self.private_key = private_key
+        self.public_keys = public_keys
+        self.highest_vote_round = highest_vote_round
+        self.highest_qc_round = highest_qc_round
+        self.sender = sender
+        self.pvt_key = pvt_key
+        self.pbc_key = pbc_key
 
-    def _increase_highest_vote_round(self, round):
-        self._highest_vote_round = max(round, self._highest_vote_round)
+    def _increase_highest_vote_round(self, roundNo):
+        self.highest_vote_round = max(roundNo, self.highest_vote_round)
 
     def _update_highest_qc_round(self, qc_round):
-        self._highest_qc_round = max(qc_round, self._highest_qc_round)
+        self.highest_qc_round = max(qc_round, self.highest_qc_round)
 
-    def _consecutive(self, block_round, round):
-        return (round + 1) == block_round
+    def _consecutive(self, block_round, roundNo):
+        return (roundNo + 1) == block_round
 
     def _safe_to_extend(self, block_round, qc_round, tc):
         return self._consecutive(block_round, tc.round) and (qc_round >= max(tc.tmo_high_qc_rounds))
 
     def _safe_to_vote(self, block_round, qc_round, tc):
-        if (block_round <= max(self._highest_vote_round, qc_round)):
+        if (block_round <= max(self.highest_vote_round, qc_round)):
             return False
         return self._consecutive(block_round, qc_round) or self._safe_to_extend(block_round, qc_round, tc)
 
-    def _safe_to_timeout(self, round, qc_round, tc):
-        if (qc_round < self._highest_qc_round) or (round <= max((self._highest_vote_round - 1), qc_round)):
+    def _safe_to_timeout(self, roundNo, qc_round, tc):
+        if (qc_round < self.highest_qc_round) or (roundNo <= max((self.highest_vote_round - 1), qc_round)):
             return False
-        return self._consecutive(round, qc_round) or self._consecutive(round, tc.round)
+        return self._consecutive(roundNo, qc_round) or self._consecutive(roundNo, tc.round)
 
     def _commit_state_id_candidate(self, block_round, qc):
         if self._consecutive(block_round, qc.vote_info.round):
-            return self._ledger.pending_state(qc.id)
+            return self.ledger.pending_state(qc.id)
         else:
             return None
     
@@ -48,33 +49,24 @@ class Safety():
         #TODO
         return True
 
-    def _hash(self, vote_info):
-        #yet to be implemented
-        return "hash of vote_info"
-
     def make_vote(self, b, last_tc):
-        qc_round = b.qc.vote_info.round
-        if self._validate_signatures(b, last_tc) and self._safe_to_vote(b.round, qc_round, last_tc):
+        qc_round = b.qc.vote_info.roundNo
+        if self._validate_signatures(b, last_tc) and self._safe_to_vote(b.roundNo, qc_round, last_tc):
             self._update_highest_qc_round(qc_round)
-            self._increase_highest_vote_round(b.round)
-            vote_info = VI.VoteInfo()
-            vote_info.id = b.id
-            vote_info.round = b.round
-            vote_info.parent_id = b.qc.vote_info.id
-            vote_info.parent_round = qc_round
-            vote_info.exec_state_id = self._ledger.pending_state(b.id)
-            ledger_commit_info = LedgerCommitInfo.LedgerCommitInfo()
-            ledger_commit_info.commit_state_id = self._commit_state_id_candidate(b.round, b.qc)
-            ledger_commit_info.vote_info_hash = self._hash(vote_info)
-            return VoteMsg.VoteMsg(vote_info, ledger_commit_info, self._blocktree.high_commit_qc)
+            self._increase_highest_vote_round(b.roundNo)
+            vote_info = bt.VoteInfo(b.id, b.roundNo, b.qc.vote_info.id, self.ledger.pending_state(b.id))
+            ledger_commit_info = bt.LedgerCommitInfo(self._commit_state_id_candidate(b.roundNo, b.qc), vote_info)
 
-    def make_timeout(self, round, high_qc, last_tc):
-        qc_round = high_qc.vote_info.round
-        if self._validate_signatures(high_qc, last_tc) and self._safe_to_timeout(round, qc_round, last_tc):
-            self._increase_highest_vote_round(round)
-            timeoutinfo = Timeoutinfo.TimeoutInfo()
-            timeoutinfo.round = round
-            timeoutinfo.high_qc = high_qc
+            return bt.VoteMsg(vote_info, ledger_commit_info, self.blocktree.high_commit_qc)
+        
+        return None
+
+    def make_timeout(self, roundNo, high_qc, last_tc):
+        qc_round = high_qc.vote_info.roundNo
+        if self._validate_signatures(high_qc, last_tc) and self._safe_to_timeout(roundNo, qc_round, last_tc):
+            self._increase_highest_vote_round(roundNo)
+            timeoutinfo = Timeoutinfo.TimeoutInfo(roundNo, high_qc, self.sender, self.pvt_key, self.pbc_key)
             return timeoutinfo
+        
         return None
 
--- src/TimeoutMsg.py
+++ src/TimeoutMsg.py
@@ -2,6 +2,7 @@ from TimeoutInfo import TimeoutInfo
 from TC import TC
 from BlockTree import QC
 
+# TODO : add a comparator operator to see if two messages are equal
 class TimeoutMsg():
     def __init__(self, tmo_info: TimeoutInfo, last_round_tc: TC, high_commit_qc: QC):
         self.tmo_info = tmo_info
