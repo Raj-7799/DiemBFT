f606962d2bf5e2842244083db5430a384b24f0d1
Vishal Singh <vishals706@gmail.com>
2021-10-14 03:13:37 +0000

    Added logger format for Pacemaker

--- src/diembft_logger.py
+++ src/diembft_logger.py
@@ -1,7 +1,7 @@
 import logging
 import sys
 from logging.handlers import TimedRotatingFileHandler
-FORMATTER = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
+FORMATTER = logging.Formatter("%(asctime)s — %(name)s — %(lineno)d  — %(levelname)s — %(message)s")
 LOG_FILE = "diem.log"
 
 def get_console_handler():
