988d2c6a8169f75f7acd40989a364a32b4d0a262
Vishal Singh <vishals706@gmail.com>
2021-10-16 23:23:27 +0000

    Added Signature verification in client_diem

--- src/Util.py
+++ src/Util.py
@@ -33,9 +33,14 @@ def sign_object(obj, pvt_key, pbc_key):
     return sign_message(seralized_msg, pvt_key, pbc_key)
 
 def sign_message(pickled_msg, pvt_key, pbc_key):    
+    
+    print("Start Signing")
     signed_hex = pvt_key.sign(pickled_msg ,encoder=HexEncoder)
     verify_key = pbc_key
-    verify_key_hex = verify_key.encode(encoder=HexEncoder)        
+    print("Signed Successful")
+    verify_key_hex = verify_key.encode(encoder=HexEncoder)
+    
+    print("Encoding Successful")     
     return [signed_hex,verify_key_hex]
 
 def check_authenticity(obj, signed_msg):
@@ -50,3 +55,34 @@ def verify_message(signed_msg):
         return verify_key.verify(signed_hex.message,signature_bytes,encoder=HexEncoder)
     except BadSignatureError:
         return None
+
+
+# def sign_object(obj, pvt_key):
+#     seralized_msg =  pickle.dumps(obj)
+#     return sign_message(seralized_msg, pvt_key)
+
+# def sign_message(pickled_msg, pvt_key):
+#     print("Start Signing")
+#     signed_hex = pvt_key.sign(pickled_msg ,encoder=HexEncoder)
+#     print("Signed Successful")
+#     return signed_hex
+
+# def encode_key(key):
+#     print("Start Encoding")
+#     key_hex = key.encode(encoder=HexEncoder)
+#     print("End Encoding")
+#     return key_hex
+
+# def check_authenticity(obj, signed_msg, pbc_key):
+#     objIdentity = verify_message(signed_msg, pbc_key)
+#     return pickle.dumps(obj) == objIdentity
+
+# def verify_message(signed_msg, pbc_key):
+#     signed_hex = signed_msg
+#     verify_key_hex = encode_key(pbc_key)
+#     signature_bytes = HexEncoder.decode(signed_hex.signature)
+#     verify_key = VerifyKey(verify_key_hex,encoder=HexEncoder)
+#     try:
+#         return verify_key.verify(signed_hex.message,signature_bytes,encoder=HexEncoder)
+#     except BadSignatureError:
+#         return None
