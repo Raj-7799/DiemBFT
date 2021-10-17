769a39b1aa8ad80d85f277290137616ada16aba5
Vishal Singh <vishals706@gmail.com>
2021-10-17 02:50:53 +0000

    Uncommenting Verification code for master merge

--- src/Util.py
+++ src/Util.py
@@ -28,33 +28,33 @@ def hash(object):
     digest = hasher(pickle.dumps(object), encoder=nacl.encoding.HexEncoder)
     return digest
 
-# def sign_object(obj, pvt_key, pbc_key):
-#     seralized_msg =  pickle.dumps(obj)
-#     return sign_message(seralized_msg, pvt_key, pbc_key)
+def sign_object(obj, pvt_key, pbc_key):
+    seralized_msg =  pickle.dumps(obj)
+    return sign_message(seralized_msg, pvt_key, pbc_key)
 
-# def sign_message(pickled_msg, pvt_key, pbc_key):    
+def sign_message(pickled_msg, pvt_key, pbc_key):    
     
-#     print("Start Signing")
-#     signed_hex = pvt_key.sign(pickled_msg ,encoder=HexEncoder)
-#     verify_key = pbc_key
-#     print("Signed Successful")
-#     verify_key_hex = verify_key.encode(encoder=HexEncoder)
+    print("Start Signing")
+    signed_hex = pvt_key.sign(pickled_msg ,encoder=HexEncoder)
+    verify_key = pbc_key
+    print("Signed Successful")
+    verify_key_hex = verify_key.encode(encoder=HexEncoder)
     
-#     print("Encoding Successful")     
-#     return [signed_hex,verify_key_hex]
+    print("Encoding Successful")     
+    return [signed_hex,verify_key_hex]
 
-# def check_authenticity(obj, signed_msg):
-#     objIdentity = verify_message(signed_msg)
-#     return pickle.dumps(obj) == objIdentity
+def check_authenticity(obj, signed_msg):
+    objIdentity = verify_message(signed_msg)
+    return pickle.dumps(obj) == objIdentity
 
-# def verify_message(signed_msg):
-#     signed_hex, verify_key_hex=signed_msg
-#     signature_bytes = HexEncoder.decode(signed_hex.signature)
-#     verify_key = VerifyKey(verify_key_hex,encoder=HexEncoder)
-#     try:
-#         return verify_key.verify(signed_hex.message,signature_bytes,encoder=HexEncoder)
-#     except BadSignatureError:
-#         return None
+def verify_message(signed_msg):
+    signed_hex, verify_key_hex=signed_msg
+    signature_bytes = HexEncoder.decode(signed_hex.signature)
+    verify_key = VerifyKey(verify_key_hex,encoder=HexEncoder)
+    try:
+        return verify_key.verify(signed_hex.message,signature_bytes,encoder=HexEncoder)
+    except BadSignatureError:
+        return None
 
 
 def sign_object_dup(obj, pvt_key):
