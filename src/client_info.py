1daf0601a7ac96532a50cdc571bc44ab335fa6fe
Raj-7799 <raj1471997@gmail.com>
2021-10-19 11:53:41 +0000

    Adding comments in client info  and client request

--- src/client_info.py
+++ src/client_info.py
@@ -1,7 +1,6 @@
-
-
 import os
 
+# Object to hold the info of the client as well as it's public key
 class ClientInfo:
     def __init__(self, public_key: str, clientID: int):
         self.public_key = public_key
